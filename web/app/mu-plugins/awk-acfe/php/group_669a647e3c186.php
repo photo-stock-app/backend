0f280130191094c6da6b26567d6352153a1ffdab
alonecat <htchtc052@gmail.com>
2024-07-19 16:06:08 +0300

    Merge branch 'main' of https://github.com/htchtc052/photo-stock-wp
    # Conflicts:
    #	.gitignore

---
+++ web/app/mu-plugins/awk-acfe/php/group_669a647e3c186.php
@@ -0,0 +1,68 @@
+<?php 
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+	'key' => 'group_669a647e3c186',
+	'title' => 'testff',
+	'fields' => array(
+		array(
+			'key' => 'field_669a647e27930',
+			'label' => 'frfr',
+			'name' => 'frfrfrfrfr',
+			'aria-label' => '',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'maxlength' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'acfe_field_group_condition' => 0,
+			'show_in_graphql' => 1,
+			'graphql_description' => '',
+			'graphql_field_name' => 'frfr',
+			'graphql_non_null' => 0,
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'gallery',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'left',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+	'show_in_rest' => 0,
+	'acfe_autosync' => array(
+		0 => 'php',
+		1 => 'json',
+	),
+	'acfe_form' => 0,
+	'show_in_graphql' => 1,
+	'graphql_field_name' => 'testff',
+	'map_graphql_types_from_location_rules' => 0,
+	'graphql_types' => '',
+	'acfe_display_title' => '',
+	'acfe_meta' => '',
+	'acfe_note' => '',
+	'modified' => 1721394331,
+));
+
+endif;
